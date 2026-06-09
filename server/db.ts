c9b2b0f67f73535a3486722d4432849b5bd7362c
ZimZum <masymalt@gmail.com>
2026-06-09 16:29:10 +0300

    подготовка к деплою

--- server/db.ts
+++ server/db.ts
@@ -1,7 +1,8 @@
 import Database from 'better-sqlite3';
 import path from 'path';
 
-const db = new Database(path.join(process.cwd(), 'db.sqlite'));
+const dbPath = process.env.DB_PATH ?? path.join(process.cwd(), 'db.sqlite');
+const db = new Database(dbPath);
 
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
