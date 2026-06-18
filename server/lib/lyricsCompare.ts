e8bbda79ad069132afee887152760a2aab6e5c2e
ZimZum <masymalt@gmail.com>
2026-06-18 16:23:43 +0300

    еще попытка с лирикой

--- server/lib/lyricsCompare.ts
+++ server/lib/lyricsCompare.ts
@@ -4,6 +4,7 @@ export function extractLyricsText(html: string): string {
   const $ = load(html);
   const parts: string[] = [];
   $('div[data-lyrics-container="true"]').each((_, el) => {
+    $(el).find('[data-exclude-from-selection="true"]').remove();
     $(el).find('br').replaceWith('\n');
     parts.push($(el).text());
   });
